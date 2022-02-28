8a0a0e6d71e76dd3baabeab8bbd8120f4adf1d83
Md-Nihal <mdnihal01716@gmail.com>
2022-03-01 03:53:27 +0600

    single detail API loaded

--- js/phone.js
+++ js/phone.js
@@ -20,15 +20,28 @@ phones.forEach(phone =>{
 const div = document.createElement('div');
 div.classList.add('col');
 div.innerHTML = `
-    <div class="card">
+    <div class="card rounded p-3">
+      <div class="p-5">
       <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
+      </div>
       <div class="card-body">
         <h3 class="card-title">Model: ${phone.phone_name}</h3>
         <h5 class="card-title"> Brand: ${phone.brand}</h5>
-        <button type="button" class="btn btn-primary btn-sm">Show Details</button>
+        <button onclick="loadDetails('${phone.slug}')" type="button" class="btn btn-primary btn-sm">Show Details</button>
       </div>
     </div>
 `
 displayFeild.appendChild(div)
 })
+}
+const loadDetails = (phone) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phone}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayDetails(data.data))
+}
+const displayDetails = details =>{
+    console.log(details);
+    const displayDetail = document.getElementById('singleDetails');
+     
 }
